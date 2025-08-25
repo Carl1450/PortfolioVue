0873d1e6447c58e92af8037c7852c30eec46b6d6
Carl <10485521@ucn.dk>
2025-08-25 16:35:42 +0200

    Emailjs now up and running

--- src/components/UI/Input.vue
+++ src/components/UI/Input.vue
@@ -9,6 +9,7 @@
     <component
       :is="type === 'textarea' ? 'textarea' : 'input'"
       :id="id"
+      :name="name"
       :rows="type === 'textarea' ? rows : undefined"
       :placeholder="placeholder"
       class="shadow-sm text-white text-sm rounded-lg block w-full p-2.5 focus:ring-primary-500 focus:border-primary-500 border border-secondary dark:bg-[#ffffff29] bg-primary"
@@ -22,5 +23,6 @@ defineProps({
   type: { type: String, default: "text" },
   placeholder: { type: String, default: "" },
   rows: { type: Number, default: 4 },
+  name: { type: String, required: true },
 });
 </script>
